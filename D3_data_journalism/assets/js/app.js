74e1ca52785399392d50fec2bbcc32822eaf8b5c
amilakanishka <amilakanishka@gmail.com>
2020-09-06 03:00:05 +1000

    added tooltip

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,8 +6,8 @@ var data;
 var chartMargin = {
     top: 30,
     right: 30,
-    bottom: 30,
-    left: 30
+    bottom: 50,
+    left: 50
   };
 
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
@@ -26,6 +26,7 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     stateData.forEach(function(data) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
       });    
 
     console.log(stateData);
@@ -41,22 +42,6 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);   
 
-    for (var i = 0; i < stateData.length; i++) {
-        chartGroup.append('g').append("circle")
-            .attr("cx",  xLinearScale(stateData[i].poverty))
-            .attr("cy", yLinearScale(stateData[i].healthcare))
-            .attr("r",10)
-            .classed("stateCircle", true);
-
-        chartGroup.append('g').append("text")
-            .attr("x",  xLinearScale(stateData[i].poverty))
-            .attr("y", yLinearScale(stateData[i].healthcare)+5)        
-            .classed("stateText", true)  
-            .attr("text-anchor","middle")        
-            .text(stateData[i].abbr)
-            .style("font-size",10);          
-    }    
-
     chartGroup.append("g")
     .classed("axis", true)
     .call(leftAxis);    
@@ -65,5 +50,61 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     .classed("axis", true)
     .attr("transform", `translate(0, ${chartHeight})`)
     .call(bottomAxis);    
+  
+    // append x axis
+    chartGroup.append("text")
+    .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top})`)
+    .classed("aText", true)
+    .text("In Poverty (%)");
+
+  // append y axis
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - chartMargin.left)
+    .attr("x", 0 - (chartHeight / 2))
+    .attr("dy", "1em")
+    .classed("aText", true)
+    .text("Lacks Healthcare (%)");      
+
+    for (var i = 0; i < stateData.length; i++) {
+        chartGroup.append('g').append("circle")
+            .attr("cx",  xLinearScale(stateData[i].poverty))
+            .attr("cy", yLinearScale(stateData[i].healthcare))
+            .attr("r",10)
+            .attr("opacity", "0.5")
+            .classed("stateCircle", true)
+
+            chartGroup.append('g').append("text")
+            .attr("x",  xLinearScale(stateData[i].poverty))
+            .attr("y", yLinearScale(stateData[i].healthcare)+5)        
+            .classed("stateText", true)  
+            .attr("text-anchor","middle")        
+            .text(stateData[i].abbr)
+            .style("font-size",10);          
+    }    
+    
+    var circlesGroup = chartGroup.selectAll("circle").data(stateData)
+
+    // Append tooltip div
+    var toolTip = d3.select("body")
+      .append("div")
+      .classed("d3-tip", true); 
+      
+    // Create "mouseover" event listener to display tooltip
+    circlesGroup.on("mouseover", function(d) {
+      toolTip.style("display", "block")
+          .html(
+            `<strong>${d.state}<strong><BR>Poverty: ${d.poverty}
+            <BR> Obesity: ${d.obesity}`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px")
+          .style("position", "absolute")
+      // Create "mouseout" event listener to hide tooltip
+      .on("mouseout", function() {
+        toolTip.style("display", "none");
+      });          
+    })      
           
+  }).catch(function(error) {
+      console.log(error);
 });
